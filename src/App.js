677edd298ab0409eaad1296f2f37a14d2caece38
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-04-08 11:11:33 -0400

    Initialized project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    )
+  }
+}
+
